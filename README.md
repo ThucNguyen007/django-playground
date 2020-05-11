0111794cc304cd57663a9a1da174db2a2cf9aadd
Thuc Nguyen <thucdlnguyen@gmail.com>
2020-05-11 18:21:55 -0400

    Create project with django and python.

---
+++ README.md
@@ -0,0 +1,22 @@
+## Usage
+## :gear: Setup your own
+
+```bash
+# Clone repo
+$ git clone git@github.com:ThucNguyen007/django-playground.git
+
+$ cd thuchome
+
+# Create a virtualenv (Optional but reccomended)
+$ virtualenv .
+
+# Install all dependencies
+$ pip install -r requirements.txt
+
+# Runserver of django backend
+$ python manage.py runserver
+
+# Open another Windows terminal after finishing compilling code
+$ python manage.py makemigrations
+$ python manage.py migrate
+
---
+++ src/pages/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_page(request, *args, **kwargs): # *args, **kwargs
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
+
+def contact_page(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_page(request, *args, **kwargs):
+    my_context = {
+        "title": "this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
+    }
+    return render(request, "about.html", my_context)
+
+def social_page(request, *args, **kwargs):
+    return HttpResponse("<h1>Socail Page</h1>")
